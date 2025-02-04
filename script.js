a3a29f2bc3a6118dda4d2d31acc5e73d937c45a2
Leandro7-moreira <leandro-denti@hotmail.com>
2025-02-04 13:04:42 -0300

    atualização bolsas 1.0

--- script.js
+++ script.js
@@ -39,166 +39,204 @@ function adicionarAoCarrinho(produto) {
 
 // Produtos aqui 
 document.addEventListener('DOMContentLoaded', function() {
-const produtos = [ 
-    {
-            nome: 'Bolsa de Crochê 1',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 199.00,
-            imagem: 'bolsas/foto 1.jpg'
-          
-        },
+    const produtos = [ 
         {
-            nome: 'Bolsa de Crochê 2',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 150.00,
-            imagem: 'bolsas/foto 2.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 3',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 160.00,
-            imagem: 'bolsas/foto 3.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 4',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 180.00,
-            imagem: 'bolsas/foto 4.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 5',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 5.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 6',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 6.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 7',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 7.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 8',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 8.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 9',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 9.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 10',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 10.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 11',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 11.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 12',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 12.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 13',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 13.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 14',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 14.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 15',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 15.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 16',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 16.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 17',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 17.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 18',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 18.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 19',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 19.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 20',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 20.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 21',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 21.jpg'
-        },
-        {
-            nome: 'Bolsa de Crochê 22',
-            descricao: 'Peça confeccionada em fio náutico.',
-            preco: 250.00,
-            imagem: 'bolsas/foto 22.jpg'
-        },
-        // Adicione mais produtos conforme necessário
-    ];
-
-    let carrinho = [];
-    const produtoLista = document.querySelector('.produtos-lista');
-
-    produtos.forEach(produto => {
-        const produtoItem = document.createElement('div');
-        produtoItem.classList.add('produto-item');
-        produtoItem.innerHTML = `
-            <img src="${produto.imagem}" alt="${produto.nome}">
-            <h3>${produto.nome}</h3>
-            <p>${produto.descricao}</p>
-            <p class="preco">R$ ${produto.preco.toFixed(2)}</p>
-            <a href="#" class="btn">Adicionar ao carrinho</a>
-        `;
-
-        const addToCartBtn = produtoItem.querySelector('.btn');
-        addToCartBtn.addEventListener('click', (e) => {
-            e.preventDefault();
-            adicionarAoCarrinho(produto);
+                nome: 'Bolsa Artemis',
+                descricao: 'Peça confeccionada em fio de malha com ferragens níquel.',
+                preco: 200.00,
+                imagem: ['bolsas/Bolsa Artemis 1.png', 'bolsas/Bolsa Artemis 2.png']
+            },
+            {
+                nome: 'Bolsa Bali',
+                descricao: 'Peça confeccionada em fio de malha com alça e ferragens douradas, versátil e elegante',
+                preco: 249.00,
+                imagem: ['bolsas/Bolsa Bali 1.png', 'bolsas/Bolsa Bali 2.png']
+            },
+            {
+                nome: 'Bolsa Carmem', 
+                descricao: 'Peça confeccionada em fio de malha com 2 alças, uma em couro sintético e ferragens douradas versátil para todas as ocasiões',
+                preco: 279.00,
+                imagem: ['bolsas/Bolsa Carmem 1.png', 'bolsas/Bolsa Carmem 2.png']
+            },
+            {
+                nome: 'Bolsa e Carteira Barbie',
+                descricao: 'Peça confeccionada em fio de malha com ferragens níquel.',
+                preco: 249.00,
+                imagem: ['bolsas/Bolsa Carteira Barbie 1.png', 'bolsas/Bolsa Carteira Barbie 2.png']
+            },
+            {
+                nome: 'Bolsa Drika',
+                descricao: 'Peça confeccionada em fio de malha e base em couro sintético com fecho de níquel',
+                preco: 250.00,
+                imagem: ['bolsas/Bolsa Drika 1.png', 'bolsas/Bolsa Drika 2.png']
+            },
+            {
+                nome: 'Bolsa Florença',
+                descricao: 'Peça confeccionada em fio náutico e ferragens dourada, espaçosa e elegante',
+                preco: 219.00,
+                imagem: ['bolsas/Bolsa Florença 1.png', 'bolsas/Bolsa Florença 2.png']
+            },
+            {
+                nome: 'Bolsa Flores',
+                descricao: 'Peça confeccionada em fio náutico e ferragens em níquel para mulheres ousadas',
+                preco: 239.00,
+                imagem: ['bolsas/Bolsa Flores 1.png', 'bolsas/Bolsa Flores 2.png']
+            },
+            {
+                nome: 'Bolsa Isis',
+                descricao: 'Peça confeccionada em fio de malha com alã em madeira',
+                preco: 129.00,
+                imagem: ['bolsas/Bolsa Isis 1.png', 'bolsas/Bolsa Isis 2.png']
+            },
+            {
+                nome: 'Bolsa Lola',
+                descricao: 'Peça confeccionada em fio de malha, alças de couro sintético e ferragens douradas',
+                preco: 269.00,
+                imagem: ['bolsas/Bolsa Lola 1.png', 'bolsas/Bolsa Lola 2.png']
+            },
+            {
+                nome: 'Bolsa Lorena',
+                descricao: 'Peça confeccionada em fio náutico com alto brilho e fecho dourado',
+                preco: 280.00,
+                imagem: ['bolsas/Bolsa Lorena 1.png', 'bolsas/Bolsa Lorena 2.png']
+            },
+            {
+                nome: 'Bolsa Magnólia',
+                descricao: 'Peça confeccionada em fio de malha com alça versáil em ferragens douradas',
+                preco: 319.00,
+                imagem: ['bolsas/Bolsa Magnólia 1.png', 'bolsas/Bolsa Magnólia 2.png']
+            },
+            {
+                nome: 'Bolsa Maia',
+                descricao: 'Peça confeccionada em fio náutico com duas alças de mão e uma tira color ferragens em níquel para mulheres elegantes e versáteis',
+                preco: 329.00,
+                imagem: ['bolsas/Bolsa Maia 1.png', 'bolsas/Bolsa Maia 2.png']
+            },
+            {
+                nome: 'Bolsa Margarida',
+                descricao: 'Peça confeccionada em fio de malha, com fecho da vovó e ferragens de níquel com três tipos de alça',
+                preco: 290.00,
+                imagem: ['bolsas/Bolsa Margarida 1.png', 'bolsas/Bolsa Margarida 2.png']
+            },
+            {
+                nome: 'Bolsa Meg',
+                descricao: 'Peça confeccionada em fio de malha com alça de mão em couro sintético e ferragens douradas',
+                preco: 260.00,
+                imagem: ['bolsas/Bolsa Meg 1.png', 'bolsas/Bolsa Meg 2.png']
+            },
+            {
+                nome: 'Bolsa Megan',
+                descricao: 'Peça confeccionada em fio de malha com ferragens níquel.',
+                preco: 299.00,
+                imagem: ['bolsas/Bolsa Megan 1.png', 'bolsas/Bolsa Megan 2.png']
+            },
+            {
+                nome: 'Bolsa Mini linha Encanto',
+                descricao: 'Peça confeccionada em fio náutico e ferragens douradas',
+                preco: 170.00,
+                imagem: ['bolsas/Bolsa Mini Linha Encanto 1.png', 'bolsas/Bolsa Mini Linha Encanto 2.png']
+            },
+            {
+                nome: 'Bolsa Miranda',
+                descricao: 'Peça confeccionada em fio em pelos com fecho da vovó e ferragens em níquel para ocasiões especiais',
+                preco: 259.00,
+                imagem: ['bolsas/Bolsa Miranda 1.png', 'bolsas/Bolsa Miranda 2.png']
+            },
+            {
+                nome: 'Bolsa Olivia',
+                descricao: 'Peça confeccionada em fio náutico com alça de madeira e ferragens douradas',
+                preco: 200.00,
+                imagem: ['bolsas/Bolsa Olivia 1.png', 'bolsas/Bolsa Olivia 2.png']
+            },
+            {
+                nome: 'Bolsa Pamela',
+                descricao: 'Peça confeccionada em fio de malha com aba base e alça em couro sintético, com ferragens douradas para todas as ocasiões',
+                preco: 229.00,
+                imagem: ['bolsas/Bolsa Pamela 1.png', 'bolsas/Bolsa Pamela 2.png']
+            },
+            {
+                nome: 'Bolsa Paris',
+                descricao: 'Peça confeccionada em fio náutico e alça de madeira com estilo e ousadia',
+                preco: 299.00,
+                imagem: ['bolsas/Bolsa Paris 1.png', 'bolsas/Bolsa Paris 2.png']
+            },
+            {
+                nome: 'Bolsa Praia',
+                descricao: 'Peça confeccionada em fio náutico.',
+                preco: 159.00,
+                imagem: ['bolsas/Bolsa Praia 1.png', 'bolsas/Bolsa Praia 2.png']
+            },
+            {
+                nome: 'Carteira Linha Encanto',
+                descricao: 'Peça confeccionada em fio náutico com laça de níquel',
+                preco: 99.00,
+                imagem: ['bolsas/Carteira Linha Encanto.png']
+            },
+            {
+                nome: 'Carteira Luna',
+                descricao: 'Peça confeccionada em cordão de algodão e bordado',
+                preco: 149.00,
+                imagem: ['bolsas/Carteira Luna 1.png', 'bolsas/Carteira Luna 2.png']
+            },
+            {
+                nome: 'Mateira e Porta Vinho',
+                descricao: 'Peça confeccionada em fio de malha com aba e alça em couro sintético, com base e divisor em acrilíco',
+                preco: 329.00,
+                imagem: ['bolsas/Mateira e Porta Vinho 1.png', 'bolsas/Mateira e Porta Vinho 2.png']
+            },
+            {
+                nome: 'Bolsa Riviera',
+                descricao: 'Peça confeccionada em fio náutico e ferragens douradas para todas as ocasiões',
+                preco: 299.00,
+                imagem: ['bolsas/Bolsa Riviera 1.png',' bolsas/Bolsa Riviera 2.png']
+            },
+            {
+                nome: 'Bolsa Melani',
+                descricao: 'Peça confeccionada em fio náutico com duas alças e ferragens douradas para mulheres estilosas',
+                preco: 299.00,
+                imagem: ['bolsas/Bolsa Melani 1.png',' bolsas/Bolsa Melani 2.png']
+            },
+            
+            // Adicione mais produtos conforme necessário
+        ];
+    
+        let carrinho = [];
+        const produtoLista = document.querySelector('.produtos-lista');
+    
+        produtos.forEach((produto, index) => {
+            const produtoItem = document.createElement('div');
+            produtoItem.classList.add('produto-item');
+            produtoItem.innerHTML = `
+                <div class="slider">
+                    <button class="prev" onclick="mudarImagem(${index}, -1)">&#10094;</button>
+                    <img src="${produto.imagem[0]}" alt="${produto.nome}" id="imagem-${index}">
+                    <button class="next" onclick="mudarImagem(${index}, 1)">&#10095;</button>
+                </div>
+                <h3>${produto.nome}</h3>
+                <p>${produto.descricao}</p>
+                <p class="preco">R$ ${produto.preco.toFixed(2)}</p>
+                <a href="#" class="btn">Adicionar ao carrinho</a>
+            `;
+    
+            const addToCartBtn = produtoItem.querySelector('.btn');
+            addToCartBtn.addEventListener('click', (e) => {
+                e.preventDefault();
+                adicionarAoCarrinho(produto);
+            });
+    
+            produtoLista.appendChild(produtoItem);
         });
-
-        produtoLista.appendChild(produtoItem);
-    });       
-});
+    
+        const indicesImagens = new Array(produtos.length).fill(0);
+    
+        window.mudarImagem = function(index, direcao) {
+            const produto = produtos[index];
+            if (produto.imagem.length > 1) {
+                indicesImagens[index] = (indicesImagens[index] + direcao + produto.imagem.length) % produto.imagem.length;
+                document.getElementById(`imagem-${index}`).src = produto.imagem[indicesImagens[index]];
+            }
+        };
+    });
    
 // Código para Slider
 document.addEventListener('DOMContentLoaded', function() {
